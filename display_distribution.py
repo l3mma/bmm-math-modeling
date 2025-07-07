4c49e72bb94743c3f3e91c062878bcdf7e376024
Vladimir <shmurvl@gmail.com>
2025-07-07 14:27:57 +0300

    fixed pieces math, no extra calculations

--- display_distribution.py
+++ display_distribution.py
@@ -1,7 +1,8 @@
 from pointGen import *
 import plotly.express as px
+import pandas as pd
 
-df = pd.DataFrame(points_inside, columns=['x', 'y', 'z'])
+df = pd.DataFrame(points, columns=['x', 'y', 'z'])
 fig = px.scatter_3d(df, x = df['x'], y = df['y'], z = df['z'])
 fig.update_traces(marker=dict(size=5))
 fig.show()
--- pointGen.py
+++ pointGen.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 
 try:
     R = int(input("Inter radius of missile: "))
@@ -14,21 +13,21 @@ except AssertionError as e:
     print("Negative number of shattered pieces")
     exit()
 
-d = input("""
+d = int(input("""
     TYPE DISTRIBUTION:
     1) GAUSSIAN
     2) POISSON
     3) UNIFORM
-""").lower()
+"""))
 
 points = []
 match d:
-    case "gaussian":
+    case 1:
         points = np.random.randn(N, 3)
-    case "poisson":
-        points = np.column_stack([np.random.poisson(R / 2, N), np.random.poisson(R / 2, N), np.random.poisson(R / 2, N)])
-    case "uniform":
-        points = np.random.uniform(low = -R, high = R, size = (N,3))
-
-distances = np.linalg.norm(points, axis=1)
-points_inside = points[distances <= R]
+    case 2:
+        x = np.random.poisson(1, N)
+        y = np.random.poisson(1, N)
+        z = np.random.poisson(1, N)
+        points = np.column_stack([x,y,z])
+    case 3:
+        points = np.random.uniform(low = 0, high = R, size = (N,3))
