e2c63a8f2b8739f15880081121200f46345add3b
Vladimir <shmurvl@gmail.com>
2025-07-08 10:17:54 +0300

    pointGen and vtkGen are merged

--- operator_vtk/markTriangles.py
+++ operator_vtk/markTriangles.py
@@ -4,29 +4,23 @@ from vtkReader import read_triangles_vtk
 import meshio
 
 all_points = []
-faces = []
-marked_triangles = []
-
 triangles = read_triangles_vtk("sphere1.vtk")
 
-all_points = []
-point_indices = {}
+point_id = {}
 
 for tr in triangles:
     for coord in tr["coords"]:
         coord_tuple = tuple(coord)
-        point_indices[coord_tuple] = len(all_points)
+        point_id[coord_tuple] = len(all_points)
         all_points.append(coord)
 
 
 points = np.array(all_points, dtype=np.float64)
-
-
 triangles = []
 is_marked = []
 
-for tri in triangles_data:
-    vertex_ids = [point_indices[tuple(coord)] for coord in tri["coords"]]
+for tri in triangles:
+    vertex_ids = [point_id[tuple(coord)] for coord in tri["coords"]]
     triangles.append(vertex_ids)
     is_marked.append(1 if tri["isMarked"] else 0)
 
