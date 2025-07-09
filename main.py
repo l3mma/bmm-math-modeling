e79dee02075327facd4881e4b2ecbd0065b9cf7b
Vladimir <shmurvl@gmail.com>
2025-07-09 08:29:09 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from operator_vtk.vtk_reader import *
+from operator_vtk.vtk_gen import rotation_z
 import ray_tracing
-from config import HEIGHT
+from config import HEIGHT, ANGLE_Z
 
 models = 'abcd.vtk'
 particles = "vtk_gen.vtk"
 
-sourse_coords = [0.0, 0.0,HEIGHT]
+source_coords = rotation_z(ANGLE_Z) @ np.array([0.0, 0.0, HEIGHT])
+
 cells = vtk_reader_cells(models)
 nodes = vtk_reader_nodes(models)
 nodes_p = vtk_reader_nodes(particles) + 3
@@ -13,6 +15,6 @@ cell_param = np.zeros(len(cells))
 
 for cell_num in range(len(cells)):
     for part_num in range(len(nodes_p)):
-        cell_param[cell_num] += ray_tracing.ray_tracing_check(sourse_coords, nodes_p[part_num], nodes[cells[cell_num][0]], nodes[cells[cell_num][1]], nodes[cells[cell_num][2]])
+        cell_param[cell_num] += ray_tracing.ray_tracing_check(source_coords, nodes_p[part_num], nodes[cells[cell_num][0]], nodes[cells[cell_num][1]], nodes[cells[cell_num][2]])
 
 print(cell_param)
