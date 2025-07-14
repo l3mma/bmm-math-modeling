a819f944de02bb93bbff70b6da16e7eedf4071cd
nd3rtoakt <I.d.poddubnyj@yandex.ru>
2025-07-14 17:14:27 +0700

    Update vtk_reader.py

--- vtk_reader.py
+++ vtk_reader.py
@@ -6,19 +6,6 @@ def vtk_reader_cells(path):
     cells = []
     for cell_block in mesh.cells:
         cells.append(cell_block.data)
-    _cells = cells[2]
-    return _cells
-
-def vtk_reader_nodes(path):
-    mesh = meshio.read(path)
-    _nodes = mesh.pointsimport meshio
-
-def vtk_reader_cells(path):
-    mesh = meshio.read(path)
-
-    cells = []
-    for cell_block in mesh.cells:
-        cells.append(cell_block.data)
     _cells = cells
     return _cells
 
@@ -36,4 +23,3 @@ def vtk_reader_point_data(path):
     mesh = meshio.read(path)
     _point_data = mesh.point_data
     return _point_data
-    return _nodes
