81ba719d5e01bd8b237fe6779ec35ad1cb79bcae
MayorIvan1 <doctorwho11120@gmail.com>
2025-07-15 14:24:17 +0700

    Add files via upload

--- vtk_writer.py
+++ vtk_writer.py
@@ -1,4 +1,8 @@
-def vtk_writer(path_particles, points, cells=[], cells_data=[], points_data=[], cells_data_name=[], points_data_name=[]):
+def vtk_writer(path_particles, points, cells=[], cells_data=[], points_data=[]):
+    len_c = len(cells)
+    len_p = len(points)
+    len_cd = len(cells_data)
+    len_pd = len(points_data)
     with open(path_particles, "w") as f:
         f.write("# vtk DataFile Version 2.0\n")
         f.write("Created by Gmsh 4.14.0\n")
@@ -10,47 +14,44 @@ def vtk_writer(path_particles, points, cells=[], cells_data=[], points_data=[], 
             f.write(f"{x} {y} {z}\n")
         f.write("\n")
 
-        if len(cells) != 0:
+        if len_c != 0:
             num_verteces = len(cells[0])
-            f.write(f"CELLS {len(cells)} {len(cells) * (num_verteces + 1)}\n")
+            f.write(f"CELLS {len_c} {len_c * (num_verteces + 1)}\n")
             for cell in cells:
                 f.write(f"{num_verteces} {cell[0]} {cell[1]} {cell[2]}\n")
             f.write("\n")
-            f.write(f"CELL_TYPES {len(cells)}\n")
-            for _ in range(len(cells)):
+            f.write(f"CELL_TYPES {len_c}\n")
+            for _ in range(len_c):
                 f.write("5\n")
 
-
-        if len(cells_data_name) != 0:
-            for num_cells_data in range(len(cells_data_name)):
-                if len(cells_data[num_cells_data]) != len(cells):
-                    raise ValueError(f"Длина массива параметров ячеек {len(cells_data[num_cells_data])} /= числу ячеек {len(cells)}")
+        if len_cd != 0:
+            for name_cd in cells_data.keys():
+                if len(cells_data[name_cd]['data']) != len_c:
+                    raise ValueError(f"Длина массива параметров ячеек {len(cells_data[name_cd]['data'])} /= числу ячеек {len_c}")
                 f.write("\n")
-                f.write(f"CELL_DATA {len(cells)}\n")
-                if len(cells_data[num_cells_data][0]) == 1:
-                    f.write(f"SCALARS {cells_data_name[num_cells_data]} float\n")
+                f.write(f"CELL_DATA {len_c}\n")
+                if cells_data[name_cd]['type'] == 'SCALARS':
+                    f.write(f"SCALARS {name_cd} float\n")
                     f.write("LOOKUP_TABLE default\n")
-                    for data in cells_data[num_cells_data]:
-                        f.write(f"{data[0]}\n")
+                    for data in cells_data[name_cd]['data']:
+                        f.write(f"{data}\n")
                 else:
-                    f.write(f"VECTORS {cells_data_name[num_cells_data]} float\n")
-                    for data in cells_data[num_cells_data]:
+                    f.write(f"VECTORS {name_cd} float\n")
+                    for data in cells_data[name_cd]['data']:
                         f.write(f"{data[0]} {data[1]} {data[2]}\n")
 
-
-        if len(points_data_name) != 0:
-            for num_points_data in range(len(points_data_name)):
-                if len(points_data[num_points_data]) != len(points):
-                    raise ValueError(
-                        f"Длина массива параметров точек {len(points_data[num_points_data])} != числу точек {len(points)}")
+        if len_pd != 0:
+            for name_pd in points_data.keys():
+                if len(points_data[name_pd]['data']) != len_p:
+                    raise ValueError(f"Длина массива параметров ячеек {len(points_data[name_pd]['data'])} /= числу ячеек {len_p}")
                 f.write("\n")
-                f.write(f"POINTS_DATA {len(points)}\n")
-                if len(points_data[num_points_data][0]) == 1:
-                    f.write(f"SCALARS {points_data_name[num_points_data]} float\n")
+                f.write(f"POINTS_DATA {len_p}\n")
+                if points_data[name_pd]['type'] == 'SCALARS':
+                    f.write(f"SCALARS {name_pd} float\n")
                     f.write("LOOKUP_TABLE default\n")
-                    for data in points_data[num_points_data]:
-                        f.write(f"{data[0]}\n")
+                    for data in points_data[name_pd]['data']:
+                        f.write(f"{data}\n")
                 else:
-                    f.write(f"VECTORS {points_data_name[num_points_data]} float\n")
-                    for data in points_data[num_points_data]:
+                    f.write(f"VECTORS {name_pd} float\n")
+                    for data in points_data[name_pd]['data']:
                         f.write(f"{data[0]} {data[1]} {data[2]}\n")
