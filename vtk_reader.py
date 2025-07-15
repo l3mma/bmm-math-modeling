17e87c87b325b970732353a7ea6ab04b3f89754b
MagyCatus <s.kraus1@yandex.ru>
2025-07-15 11:56:20 +0700

    Update vtk_reader.py

--- vtk_reader.py
+++ vtk_reader.py
@@ -2,7 +2,7 @@ import meshio
 
 def vtk_reader(path):
     mesh = meshio.read(path)
-    
+
     _nodes = mesh.points
     _cell_data = mesh.cell_data
     _point_data = mesh.point_data
@@ -10,6 +10,10 @@ def vtk_reader(path):
     cells = []
     for cell_block in mesh.cells:
         cells.append(cell_block.data)
-    _cells = cells[2]
-    
+
+    _cells = []
+    for i in cells:
+        if i.shape[1] == 3: # for triangle
+            _cells = i
+
     return _cells, _nodes, _cell_data, _point_data
