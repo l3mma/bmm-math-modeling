88db891bb3f361d0395d9bdb1be029c8df993c7d
MagyCatus <s.kraus1@yandex.ru>
2025-07-16 17:03:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ from config_reader import parse_config
 from area_calculation import interaction_area
 from combiner import conbine_models
 
-
 VERSION = "betta_0.1.0"
-
 logging.basicConfig(filename='info.log', level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 parser = argparse.ArgumentParser(description="Пример программы с версией")
@@ -85,7 +83,6 @@ for cloud in clouds:
         raise ValueError("Неизвестная форма облака частиц")
 print('Облако частиц успешно создано!')
 
-# models = []
 cells, nodes, _, _ = vtk_reader(models_params[0]["path"])
 orginal_model_p = {}
 orginal_model_p[models_params[0]["path"].split('\\')[-1]] = {'num_cell': len(cells), 'num_nodes': len(nodes)}
@@ -100,14 +97,12 @@ point_data_scal_c = np.zeros((num_cloud_particles, 1))
 
 cell_data = {}
 cell_param_all = np.zeros((len(cells), 1))
-#print('cell', cells)
-# print('nodes', nodes)
 
-# print(orginal_model_p)
 start_time = time.time()
 num_cloud = 0
 index_cloud = 0
 print('Начат процесс пересечения частиц и моделей!')
+
 for cloud in particles:
     cell_param = np.zeros((len(cells), 1))
     for part_num in range(len(cloud)):
@@ -130,7 +125,7 @@ for cloud in particles:
     num_cloud += 1
     cell_data = dict_param(cell_data, f'cloud_{num_cloud}', cell_param)
     index_cloud += len(cloud)
-    # vtk_writer(f'B:\\GMM_2025\\v5\\bmm-math-modeling-MayorIvan1-patch-1\\clouds-{num_cloud}.vtk', cloud)
+    
 print('Обработка процесса пересечения частиц и моделей успешно выполнено!')
 point_data = {}
 point_data_vec_m = np.zeros((len(nodes), 3))
@@ -142,21 +137,16 @@ point_data = dict_param(point_data, 'interaction', point_data_scal)
 
 cell_data = dict_param(cell_data, f'cloud_all', cell_param_all)
 
-
 points_cloud = []
 points_all = np.vstack([nodes, particles[0]])
 for cloud in range(1,len(particles)):
     points_all = np.vstack([points_all, particles[cloud]])
-
 
 point_data_scal_m = np.zeros((len(nodes), 1))
 point_data_scal_c = np.ones((num_cloud_particles, 1))
 point_data_scal = np.vstack([point_data_scal_m, point_data_scal_c])
 point_data = dict_param(point_data, 'mass_point', point_data_scal)
 
-
-# color_cells = {}
-# color_cells = dict_param(color_cells, 'color', cell_param)
 vtk_writer(path_dir, points_all, cells, cell_data, point_data)
 
 all_area = interaction_area(nodes, cells)
